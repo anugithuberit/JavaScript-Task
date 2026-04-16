24411d72d66f7f242d92cd73a75aca3c6bb585c7
PETCHIAMMAL MURUGAN <anumurugan544@gmail.com>
2026-04-16 13:15:51 +0530

    Update orderRouter20.js

--- Month2/Task4/express REST API/routes/orderRouter20.js
+++ Month2/Task4/express REST API/routes/orderRouter20.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-// sample route
 router.get('/', (req, res) => {
   res.json({ message: "Orders API working" });
 });
 
-// ✅ export should be LAST LINE
 module.exports = router;
